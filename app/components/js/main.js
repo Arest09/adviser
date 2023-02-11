83d418f51dede7b8aec0409fdf511875a779e43d
arest <arest09@mail.ru>
2023-02-11 22:33:30 +1000

    добавил gulp include

--- app/components/js/main.js
+++ app/components/js/main.js
@@ -1,11 +1,11 @@
 "use strict"
-const currentUrl = window.location
+/* const currentUrl = window.location
 
 let menu = document.querySelector(".menu")
 
 function home(e, currentUrl) {
-  if (e.target.closest(".menu__list-link") && currentUrl.pathname !== "/") {
-    window.location.href = "/"
+  if (e.target.closest(".menu__list-link") && currentUrl.href !== currentUrl.origin) {
+    window.location.href = window.location.origin
   }
 }
 
@@ -13,4 +13,4 @@ menu.addEventListener("click", (e) => {
   home(e, currentUrl)
 })
 
-console.log(currentUrl.pathname)
+console.log(currentUrl) */
